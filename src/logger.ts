72d0ec23a89bfc527bf7a4fb447bbb267b1b8e75
bluecomm <bluecomm42@gmail.com>
2020-12-06 16:49:15 -0600

    feat: switch to Winston logging with custom trace middleware

--- src/logger.ts
+++ src/logger.ts
@@ -1,147 +1,78 @@
-import logfmt from "logfmt";
+import winston, { Logger } from "winston";
+import * as cls from "cls-hooked";
 import { v4 as uuid } from "uuid";
 import { EventEmitter } from "events";
-import * as cls from "cls-hooked";
+import { SyslogTransportInstance } from "winston-syslog";
+import { Transports as WinstonTransports } from "winston/lib/winston/transports";
+import TransportStream from "winston-transport";
+
+import "winston-syslog";
+
+type Transports = WinstonTransports & { Syslog: SyslogTransportInstance };
+const transports = winston.transports as Transports;
 
 const clsNamespace = cls.createNamespace("clsLoggerNamespace");
 
-/**
- * A logger object.
- */
-export class Logger {
-  /** A context to apply to all log messages. */
-  context: object;
+function getTransports(): TransportStream[] {
+  const tps: TransportStream[] = [
+    new transports.Console({
+      format: winston.format.combine(addTrace(), winston.format.simple()),
+    }),
+  ];
 
-  /**
-   * Create a new logger.
-   *
-   * @param context An optional context to apply to all messages.
-   */
-  constructor(context: object = {}) {
-    this.context = context;
+  const host = process.env.SYSLOG_HOST;
+  const port = parseInt(process.env.SYSLOG_PORT);
+  if (process.env.NODE_ENV === "production" && !!host && !!port) {
+    tps.push(
+      new transports.Syslog({
+        host,
+        port,
+        protocol: "tls4",
+        localhost: `u/${process.env.REDDIT_USER}`,
+        eol: "\n",
+        format: winston.format.combine(addTrace(), winston.format.json()),
+      })
+    );
   }
 
-  /**
-   * Create a new logger that inherits it's parent's context.
-   *
-   * @param context An optional context to extend the parent.
-   *
-   * @returns The new child logger.
-   */
-  child(context: object = {}): Logger {
-    // Allow child context to override parent context.
-    return new Logger({ ...context, ...this.context });
-  }
-
-  /**
-   * Log a generic message.
-   *
-   * @param lvl The level to log at.
-   * @param msg The message to log.
-   * @param context The optional context for this message.
-   */
-  log(lvl: string, msg: string | Error, context: object | Error = {}): void {
-    if (context instanceof Error) {
-      context = {
-        error: context,
-        stack: context.stack,
-      };
-    }
-
-    if (msg instanceof Error) {
-      context = {
-        error: msg,
-        stack: msg.stack,
-        ...context,
-      };
-      msg = msg.message;
-    }
-
-    // Allow msg context to override parent context.
-    context = { ...context, ...this.context };
-
-    // Add trace UUID, if present.
-    const trace_uuid = clsNamespace.get("trace_uuid");
-    if (trace_uuid != null) {
-      context = { ...context, trace_uuid };
-    }
-
-    const data = logfmt.stringify(context);
-    console.log(`[${lvl}] ${msg} ${data}`);
-  }
-
-  /**
-   * Log a fatal message.
-   *
-   * @param msg The message to log.
-   * @param context The optional context for this message.
-   */
-  fatal(msg: string | Error, context: object = {}): void {
-    this.log("fatal", msg, context);
-  }
-
-  /**
-   * Log an error message.
-   *
-   * @param msg The message to log.
-   * @param context The optional context for this message.
-   */
-  error(msg: string | Error, context: object = {}): void {
-    this.log("error", msg, context);
-  }
-
-  /**
-   * Log a warning message.
-   *
-   * @param msg The message to log.
-   * @param context The optional context for this message.
-   */
-  warn(msg: string | Error, context: object = {}): void {
-    this.log("warn", msg, context);
-  }
-
-  /**
-   * Log an info message.
-   *
-   * @param msg The message to log.
-   * @param context The optional context for this message.
-   */
-  info(msg: string | Error, context: object = {}): void {
-    this.log("info", msg, context);
-  }
-
-  /**
-   * Log a debug message.
-   *
-   * @param msg The message to log.
-   * @param context The optional context for this message.
-   */
-  debug(msg: string | Error, context: object = {}): void {
-    this.log("debug", msg, context);
-  }
-
-  /**
-   * Log a trace message.
-   *
-   * @param msg The message to log.
-   * @param context The optional context for this message.
-   */
-  trace(msg: string | Error, context: object = {}): void {
-    this.log("trace", msg, context);
-  }
+  return tps;
 }
 
-const log = new Logger();
+const addTrace = winston.format((info, opts) => {
+  // Add trace UUID, if present.
+  const _trace = clsNamespace.get("_trace");
+  if (_trace != null) {
+    info = { ...info, _trace };
+  }
+  return info;
+});
+
+/** The default logger. */
+const log = winston.createLogger({
+  level: "info",
+  transports: getTransports(),
+});
 export default log;
 
+/**
+ * Bind one or more emitters to the CLS logging namespace.
+ *
+ * @param emitters The emitters to bind.
+ */
 export function clsBind(...emitters: EventEmitter[]): void {
   for (const emitter of emitters) {
     clsNamespace.bindEmitter(emitter);
   }
 }
+
+/**
+ * Run an async function call in the CLS logging namespace.
+ *
+ * @param fn The function to run.
+ */
 export async function clsWrap<T>(fn: () => Promise<T>): Promise<T> {
   return clsNamespace.runPromise(() => {
-    clsNamespace.set("trace_uuid", uuid());
+    clsNamespace.set("_trace", uuid());
     return fn();
   });
 }
